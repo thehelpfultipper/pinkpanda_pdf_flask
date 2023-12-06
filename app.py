369870b82cc8cfb732cff1a350272e14350ac6b6
THT <thehelpfultipper@gmail.com>
2023-12-06 11:37:23 -0800

    Testing fix on path

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 # Specify the path to the Tesseract executable
 # pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 venv_path = os.path.dirname(sys.executable)
-tesseract_path = os.path.join(venv_path, "bin", "tesseract")
+tesseract_path = os.path.join(venv_path, "tesseract")
 pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
 def highlight_exact_matches(screenshot, search_phrase, near_matches_text):
