d73ddda5d610fb071bba8186ebe8962331c1cc22
THT <thehelpfultipper@gmail.com>
2023-12-08 10:44:44 -0800

    workaround

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # backend/app.py
 from fuzzywuzzy import fuzz
 import pytesseract
-import os
+import os, sys
 from flask import Flask, request, jsonify, send_from_directory
 import fitz  # PyMuPDF
 from PIL import Image, ImageDraw
@@ -17,7 +17,10 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 # Specify the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+# pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/tessdata/bin/tesseract'
+
+print(sys.path)
 
 def highlight_exact_matches(screenshot, search_phrase, near_matches_text):
     """
